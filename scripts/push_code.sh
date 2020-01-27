da0b06211218f0b411fde6fa8492df90b810e408
tbutch <tristan.bouchard@mail.mcgill.ca>
2020-01-27 13:50:26 -0500

    Modify script to add help screen

--- scripts/push_code.sh
+++ scripts/push_code.sh
@@ -9,14 +9,21 @@ parentdir="$(dirname "$currDir")"
 
 showMenu() {
     echo "-------------------------------------------------------------"
-    echo "                       MGCIL MACHINE"
+    echo "                       PUSH TO SERVER"
     echo "-------------------------------------------------------------"
     echo "[1] push source folder to dest"
+    echo "[2] help"
     echo "[4] exit"
     echo "-------------------------------------------------------------"
 
     read -p "option: " mc
     return $mc
+}
+
+displayHelp() {
+    echo "Configure the script destination and source in this script."
+    echo "This script copies its parent directory to the specified"
+    echo "destination on the source system."
 }
 
 while [[ "$m" != "4" ]]
@@ -25,7 +32,10 @@ do
         echo "connecting to " $dest "..."
         echo "copying " "$parentdir" to "$usr"@"$dest:$destDir"
         sshpass -p "$pwd" scp -rq "$parentdir" $usr@$dest:$destDir
+    elif [[ "$m" == "2" ]]; then
+        displayHelp
     fi
+    #clear
     showMenu
     m=$?
 
