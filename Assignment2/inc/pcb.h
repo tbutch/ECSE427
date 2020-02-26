87029b26cdb6f1d7f827008163697f76b4c4686e
tbutch <tristan.bouchard@mail.mcgill.ca>
2020-02-25 19:26:37 -0500

    Add queue structs and basic queue code to PCB

--- Assignment2/inc/pcb.h
+++ Assignment2/inc/pcb.h
@@ -7,4 +7,19 @@ typedef struct{
     int end;
 } PCB_t;
 
+// Define PCB node
+typedef struct{
+    PCB_t * pcb;
+    PCB_t * next;
+} PCB_Node_t;
+
+// Define PCB linked list
+typedef struct{
+    PCB_Node_t * head;
+    PCB_Node_t * tail;
+} PCB_LinkedList;
+
+bool enqueuePCB(PCB_LinkedList * list, PCB_Node_t * node);
+bool clearReadyQueue(PCB_LinkedList * list);
+
 #endif
--- Assignment2/src/pcb.c
+++ Assignment2/src/pcb.c
@@ -1,5 +1,94 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "..\inc\pcb.h"
 
+// Private function prototypes
+bool disposeNode(PCB_Node_t *nodeToDispose);
+
+/*
+ * Function: addPCBToReadyQueue
+ * -----------------------------------------------------------------------
+ *  Function used to add a node to the PCB ready queue.
+ * 
+ *  Returns: true on success;
+ */
+bool enqueuePCB(PCB_LinkedList *list, PCB_Node_t *node) {
+    if (list == NULL || node == NULL) {
+        return false;
+    }
+    PCB_Node_t *currNode = list->head;
+    while (currNode != NULL) {
+        currNode = currNode->next;
+    }
+    currNode = node;
+    list->tail = node;
+    return true;
+}
+
+/*
+ * Function: clearReadyQueue
+ * -----------------------------------------------------------------------
+ *  Function used to clear the PCB ready queue
+ * 
+ *  Returns: 1 on success, 0 error;
+ */
+bool clearReadyQueue(PCB_LinkedList *list) {
+    if (list == NULL) {
+        return false;
+    }
+    PCB_Node_t *node;
+    while (list->head != NULL) {
+        node = list->head;
+        list->head = list->head->next;
+        if (!disposeNode(node)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+ * Function: initPCBReadyQueue
+ * -----------------------------------------------------------------------
+ *  Function used to initialize the PCB Ready queue
+ * 
+ *  Returns: true on success;
+ */
+bool initPCBReadyQueue(PCB_LinkedList *list) {
+    if (list == NULL) {
+        return false;
+    }
+    list->head = NULL;
+    list->tail = NULL;
+    return true;
+}
+
+/*
+ * Function: disposeNode
+ * -----------------------------------------------------------------------
+ *  Function used to dispose of PCB_Node_t
+ * 
+ *  Returns: True on success
+ */
+bool disposeNode(PCB_Node_t *nodeToDispose) {
+    if (nodeToDispose == NULL) {
+        return false;
+    }
+    free(nodeToDispose->pcb);
+    free(nodeToDispose);
+    return true;
+}
+
+/*
+ * Function: dequeuePCB 
+ * -----------------------------------------------------------------------
+ *  Function used to remove first item from the ready queue
+ * 
+ *  Returns: void
+ */
+PCB_Node_t * dequeuePCB(PCB_LinkedList * list){
+    PCB_Node_t * nodeToReturn = list->head;
+    list->head=list->head->next;
+}
